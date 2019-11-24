e6fba417cda46164e4b87096e0a6409b8ac3c554
Rishabh Rai <rishabh.rai@utexas.edu>
2019-11-24 00:59:58 -0600

    updated py to match ipynb

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 # 
 # We begin by converting the arguments dataset into a format useful to us. The dataset contains various features, but we're primarily concerned with creating parent-child (statement-response or input-output) post pairs. Because the posts are not necessarily replying to their direct predecessor in the discussion (and thus in the JSON), we will have to keep track of the post id and parent id for each post to create pairings.
 
-# In[ ]:
+# In[1]:
 
 
 import json
@@ -18,7 +18,7 @@ import time
 import pickle
 
 
-# In[ ]:
+# In[2]:
 
 
 id_to_text = {}
@@ -63,7 +63,7 @@ if build_pairs:
 
 # # 2. Convert raw input-output pairs into feedable format for training
 
-# In[ ]:
+# In[3]:
 
 
 import spacy
@@ -75,7 +75,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# In[ ]:
+# In[4]:
 
 
 with open(os.path.join(my_data_dir, 'id_to_text.pkl'), 'rb') as id_to_text_file:
@@ -85,7 +85,7 @@ with open(os.path.join(my_data_dir, 'parent_to_child_ids.pkl'), 'rb') as parent_
     parent_to_child_ids = pickle.load(parent_to_child_ids_file)
 
 
-# In[ ]:
+# In[5]:
 
 
 post_lengths = {post_id: len(post.split(' ')) for post_id, post  in id_to_text.items()}
@@ -95,12 +95,13 @@ print("Min post length: {}".format(np.min(list(post_lengths.values()))))
 print("Max post length: {}".format(np.max(list(post_lengths.values()))))
 print("Post length standard deviation: {}".format(np.std(list(post_lengths.values()))))
 
-print("Percentage of posts shorter than 450 words: {}".format(np.mean([post_length < 450 for post_length in post_lengths.values()])))
+max_post_length = 200
+print("Percentage of posts shorter than {} words: {}".format(max_post_length, np.mean([post_length < max_post_length for post_length in post_lengths.values()])))
 plt.hist(list(post_lengths.values()))
 plt.show()
 
 
-# In[ ]:
+# In[6]:
 
 
 post_length_ratios = [post_lengths[parent_id]/post_lengths[child_id] for parent_id, child_id in parent_to_child_ids]
@@ -110,19 +111,21 @@ print("Min ratio: {}".format(np.min(post_length_ratios)))
 print("Max ratio: {}".format(np.max(post_length_ratios)))
 print("Ratio standard deviation: {}".format(np.std(post_length_ratios)))
 
-print("Percentage of ratios greater than 4: {}".format(np.mean([post_length_ratio > 4 for post_length_ratio in post_length_ratios])))
-print("Percentage of ratios less than 0.25: {}".format(np.mean([post_length_ratio < 0.25 for post_length_ratio in post_length_ratios])))
+max_post_length_ratio = 3
+min_post_length_ratio = 0.33
+print("Percentage of ratios greater than {}: {}".format(max_post_length_ratio, np.mean([post_length_ratio > max_post_length_ratio for post_length_ratio in post_length_ratios])))
+print("Percentage of ratios less than {}: {}".format(min_post_length_ratio, np.mean([post_length_ratio < min_post_length_ratio for post_length_ratio in post_length_ratios])))
 plt.hist(post_length_ratios)
 plt.show()        
 
 
-# In[ ]:
+# In[7]:
 
 
 nlp = spacy.load("en_core_web_sm")
 
 
-# In[ ]:
+# In[8]:
 
 
 def light_tokenizer(post):
@@ -131,13 +134,10 @@ def light_tokenizer(post):
 post_field = torchtext.data.Field(tokenize=light_tokenizer, init_token='<sos>', eos_token='<eos>', lower=True)
 
 
-# In[ ]:
+# In[9]:
 
 
 build_csv = True
-max_post_length = 450
-max_post_length_ratio = 4
-min_post_length_ratio = 0.25
 
 if build_csv:
     with open(os.path.join(my_data_dir, 'id_to_text.pkl'), 'rb') as id_to_text_file:
@@ -151,7 +151,7 @@ if build_csv:
         parent_post_length = len(id_to_text[parent_post_id].split(' '))
         child_post_length = len(id_to_text[child_post_id].split(' '))
         
-        if parent_post_length < max_post_length and child_post_length < max_post_length:
+        if parent_post_length < max_post_length and child_post_length < max_post_length and parent_post_length/child_post_length <= max_post_length_ratio and parent_post_length/child_post_length >= min_post_length_ratio:
             # TODO: check for large difference in post length
             pairs['parent_post'].append(id_to_text[parent_post_id])
             pairs['child_post'].append(id_to_text[child_post_id])
@@ -164,7 +164,7 @@ if build_csv:
     test.to_csv(os.path.join(my_data_dir, 'test.csv'), index=False)
 
 
-# In[ ]:
+# In[10]:
 
 
 print("Building datasets...")
@@ -180,29 +180,29 @@ print("Vocabulary built! Time elapsed: {}".format(time.time()-vocab_start_time))
 print("Vocabulary size: {}".format(len(post_field.vocab)))
 
 
-# In[ ]:
+# In[11]:
 
 
 import torch
 
 
-# In[ ]:
+# In[12]:
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 cpu_device = torch.device('cpu')
-train_set_iterator, test_set_iterator = torchtext.data.BucketIterator.splits((train_set, test_set), batch_size=32, device=device, shuffle=True, sort_key=lambda pair: len(pair.parent_post))
+train_set_iterator, test_set_iterator = torchtext.data.BucketIterator.splits((train_set, test_set), batch_size=16, device=device, shuffle=True, sort_key=lambda pair: len(pair.parent_post))
 
 
 # # 3. Create the seq2seq model
 
-# In[ ]:
+# In[13]:
 
 
 import random
 
 
-# In[ ]:
+# In[14]:
 
 
 class Seq2SeqEncoder(torch.nn.Module):
@@ -254,30 +254,27 @@ class Seq2Seq(torch.nn.Module):
         return s2s_outputs
 
 
-# In[ ]:
+# In[15]:
 
 
 # Hyperparameters
 input_size = output_size = len(post_field.vocab)
-embedding_size = 256
-hidden_layer_size = 512
+embedding_size = 128
+hidden_layer_size = 256
 num_hidden_layers = 2
 dropout_rate = 0.5
 
+# print(torch.cuda.memory_allocated(device=0)/(10**9))
 encoder_net = Seq2SeqEncoder(input_size, embedding_size, hidden_layer_size, num_hidden_layers, dropout_rate)
+# print(torch.cuda.memory_allocated(device=0)/(10**9))
 decoder_net = Seq2SeqDecoder(input_size, embedding_size, hidden_layer_size, num_hidden_layers, output_size, dropout_rate)
+# print(torch.cuda.memory_allocated(device=0)/(10**9))
 seq2seq_net = Seq2Seq(encoder_net, decoder_net).to(device)
 
 
 # # 4. Train the seq2seq model
 
-# In[ ]:
-
-
-seq2seq_net = seq2seq_net.to(device)
-
-
-# In[ ]:
+# In[16]:
 
 
 optimizer = torch.optim.Adam(seq2seq_net.parameters())
@@ -285,19 +282,38 @@ padding_index = post_field.vocab.stoi['<pad>']
 loss_function = torch.nn.CrossEntropyLoss(ignore_index=padding_index)
 
 
-# In[ ]:
+# In[17]:
+
+
+models_directory = os.path.join(os.getcwd(), my_data_dir, 'models/')
+if not os.path.isdir(models_directory):
+    os.mkdir(models_directory)
+
+run_name = "v3-small"
+run_models_directory = os.path.join(os.getcwd(), my_data_dir, 'models/{}'.format(run_name))
+if not os.path.isdir(run_models_directory):
+    os.mkdir(run_models_directory)
+
+
+# In[18]:
 
 
 max_clip = 1
 def train(seq2seq_net, train_set_iterator, optimizer, loss_function, max_clip, writer, epoch_num):
     seq2seq_net.train()
     total_loss = 0
+    time_train = time.time()
+    inner_min_loss = float('inf')
+    iterator_length = len(train_set_iterator)
     for i, batch in enumerate(train_set_iterator):
         parent_post = batch.parent_post
         child_post = batch.child_post
-        
+        if i % 10 == 0:
+            print('train: {} of {} in epoch {}'.format(i, iterator_length, epoch_num))
+#         print('1', torch.cuda.memory_allocated(device=0)/(10**9))
         optimizer.zero_grad()
         response = seq2seq_net(parent_post, child_post, 0.5)
+#         print('2', torch.cuda.memory_allocated(device=0)/(10**9))
         response = response[1:].view(-1, response.shape[-1])
         child_post = child_post[1:].view(-1)
         
@@ -306,46 +322,52 @@ def train(seq2seq_net, train_set_iterator, optimizer, loss_function, max_clip, w
         torch.nn.utils.clip_grad_norm_(seq2seq_net.parameters(), max_clip)
         optimizer.step()
         total_loss += loss.item()
-        
-        if i % (len(train_set_iterator) // 10) == 0:
-            writer.add_scalar('Loss/train', loss.detach().to(cpu_device).numpy().squeeze(), epoch_num*len(train_set_iterator)+i)
+        inner_loss_scalar = loss.detach().to(cpu_device).numpy().squeeze()
+#         print('3', torch.cuda.memory_allocated(device=0)/(10**9))
+#         response = response.detach()
+#         del response
+        if i % (len(train_set_iterator) // 10) == 0 or i == len(train_set_iterator) - 1:
+            writer.add_scalar('Loss/train', inner_loss_scalar, epoch_num*len(train_set_iterator)+i)
+            if (inner_loss_scalar < inner_min_loss):
+                torch.save(seq2seq_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-inner-model-train-{}.pt'.format(run_name, epoch_num)))
+                torch.save(encoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-encoder-inner-model-train-{}.pt'.format(run_name, epoch_num)))
+                torch.save(decoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-decoder-inner-model-train-{}.pt'.format(run_name, epoch_num)))
+                lowest_loss_train = inner_loss_scalar
+            
+        print("total time for epoch so far: {}".format(time.time() - time_train))
     
     return total_loss/len(train_set_iterator)
 
 def test(seq2seq_net, test_set_iterator, loss_function, writer, epoch_num):
     seq2seq_net.eval()
     total_loss = 0
+    time_test = time.time()
+    iterator_length = len(test_set_iterator)
     for i, batch in enumerate(test_set_iterator):
         parent_post = batch.parent_post
         child_post = batch.child_post
         
+        if i % 10 == 0:
+            print('test: {} of {} in epoch {}'.format(i, iterator_length, epoch_num))
+#         print('1', torch.cuda.memory_allocated(device=0)/(10**9))
         response = seq2seq_net(parent_post, child_post, 0)
+#         print('2', torch.cuda.memory_allocated(device=0)/(10**9))
         response = response[1:].view(-1, response.shape[-1])
         child_post = child_post[1:].view(-1)
         
         loss = loss_function(response, child_post)
         total_loss += loss.item()
-        
+        print("total time for epoch so far: {}".format(time.time() - time_train))
+#         response = response.detach()
+#         del response
+#         print('3', torch.cuda.memory_allocated(device=0)/(10**9))
         if i % (len(test_set_iterator) // 10) == 0:
             writer.add_scalar('Loss/test', loss.detach().to(cpu_device).numpy().squeeze(), epoch_num*len(test_set_iterator)+i)
-    
+        
     return total_loss/len(test_set_iterator)
 
 
-# In[ ]:
-
-
-models_directory = os.path.join(os.getcwd(), my_data_dir, 'models/')
-if not os.path.isdir(models_directory):
-    os.mkdir(models_directory)
-
-run_name = "v0-testing"
-run_models_directory = os.path.join(os.getcwd(), my_data_dir, 'models/{}'.format(run_name))
-if not os.path.isdir(run_models_directory):
-    os.mkdir(run_models_directory)
-
-
-# In[ ]:
+# In[19]:
 
 
 import torch.utils.tensorboard
@@ -359,7 +381,7 @@ num_epochs = 10
 
 lowest_loss_train = float('inf')
 lowest_loss_test = float('inf')
-
+print(torch.cuda.memory_allocated(device=0)/(10**9))
 print("Beginning training...")
 training_start_time = time.time()
 for epoch in range(num_epochs):
@@ -367,20 +389,26 @@ for epoch in range(num_epochs):
     
     epoch_start_time = time.time()
     loss_train = train(seq2seq_net, train_set_iterator, optimizer, loss_function, max_clip, writer, epoch)
+    print("epoch train", torch.cuda.memory_allocated(device=0)/(10**9))
     loss_test = test(seq2seq_net, test_set_iterator, loss_function, writer, epoch)
+    print("epoch test", torch.cuda.memory_allocated(device=0)/(10**9))
     loss_train_scalar = loss_train.detach().to(cpu_device).numpy().squeeze()
     loss_test_scalar = loss_test.detach().to(cpu_device).numpy().squeeze()
-    writer.add_scalar('EpochLoss/train', loss_train_scalar, epoch)
-    writer.add_scalar('EpochLoss/test', loss_test_scalar, epoch)
+    writer.add_scalar('Loss/train', loss_train_scalar, epoch)
+    writer.add_scalar('Loss/test', loss_test_scalar, epoch)
     
     print("Epoch training duration: {}".format(time.time()-epoch_start_time))
     print("Updating models...")
     
     if loss_train_scalar <= lowest_loss_train:
-        torch.save(seq2seq_net.state_dict(), os.path.join(my_data_dir, '/models/{}/best-model-train.pkl'.format(run_name)))
+        torch.save(seq2seq_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-model-train.pt'.format(run_name)))
+        torch.save(encoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-encoder-model-train.pt'.format(run_name)))
+        torch.save(decoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-decoder-model-train.pt'.format(run_name)))
         lowest_loss_train = loss_train_scalar
     if loss_test_scalar <= lowest_loss_test:
-        torch.save(seq2seq_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-model-test.pkl'.format(run_name)))
+        torch.save(seq2seq_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-model-test.pt'.format(run_name)))
+        torch.save(encoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-encoder-model-test.pt'.format(run_name)))
+        torch.save(decoder_net.state_dict(), os.path.join(my_data_dir, 'models/{}/best-decoder-model-test.pt'.format(run_name)))
         lowest_loss_test = loss_test_scalar
     
     print("Total elapsed time: {}".format(time.time()-training_start_time))
